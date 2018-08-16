fcfb9fd8bc409b2cf9ea59fadf3485baecb77595
george <george@wayne.co.in>
2018-08-16 19:59:18 +0530

    l

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
 var firstName = 'John Go fuckyourself';
 console.log(firstName);
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yeahMark = now - ageMark;
+
+console.log('Johns age is' + ' '+ yearJohn);
+console.log('Marks age is'+ ' '+ yeahMark);
+console.log('Two years in the future we will be in '+ "now+2");
+console.log(now * 2);
+console.log(now / 10);
