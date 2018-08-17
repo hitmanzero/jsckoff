0cfbd4ce75bc27b067834fdcfa256a6c654af643
george <george@wayne.co.in>
2018-08-17 13:31:27 +0530

    Dibovi

--- master_react/src/index.js
+++ master_react/src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 const App = ()   => {
-    return <h1>Hello there!! General H1</h1>
+    return (
+        <div>
+        <h1>Hello there !!</h1>
+        <div>General Divobi</div>    
+        <div>General Divobi</div>  
+        <div>General Divobi</div>  
+        <div>General Divobi</div>  
+        <div>General Divobi</div>  
+        <div>General Divobi</div>  
+        </div>
+    )
+      
+
+
 }
+
+
+
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
