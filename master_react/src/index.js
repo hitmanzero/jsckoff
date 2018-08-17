d3a7b4e3e2a23a6425743a1a0b236e52748bd323
george <george@wayne.co.in>
2018-08-17 13:19:10 +0530

    phew

---
+++ master_react/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = ()   => {
+    return <h1>Hello there!! General H1</h1>
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
